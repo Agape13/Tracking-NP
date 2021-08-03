1dc6012597a7047cc6fe694e723b7be2dded48d2
Agape13 <83903366+Agape13@users.noreply.github.com>
2021-08-03 09:04:10 +0200

    Update trackingNP.py

--- trackingNP.py
+++ trackingNP.py
@@ -36,21 +36,28 @@ def reqtracks(tbox, rbox):
     tracks = tbox.get('1.0', 'end').split()
     tbox.delete('1.0', 'end')
     tbox.insert(END, '\n\n'.join(tracks))
+    rbox.config(state=NORMAL)
     rbox.delete('1.0', 'end')
     rbox.insert(END, '\n\n'.join(apirequest(tracks)))
+    rbox.config(state=DISABLED)
 
 win=Tk()
 setwindow(win)
 
+tracklabel = Label(win, text='Номери ТТН', font='Tahoma 10', bg='#eef', fg='#000')
+tracklabel.place(relx=0.05, rely=0.02, anchor='nw', relwidth=0.25)
+statuslabel = Label(win, text='Статуси', font='Tahoma 10', bg='#eef', fg='#000')
+statuslabel.place(relx=0.95, rely=0.02, anchor='ne', relwidth=0.6)
+
 trackbox = Text(win)
-trackbox.place(relx=0.05, rely=0.05, anchor='nw', relwidth=0.25, relheight=0.8)
-resultbox = Text(win)
-resultbox.place(relx=0.95, rely=0.05, anchor='ne', relwidth=0.6, relheight=0.8)
+trackbox.place(relx=0.05, rely=0.1, anchor='nw', relwidth=0.25, relheight=0.75)
+resultbox = Text(win, state=DISABLED)
+resultbox.place(relx=0.95, rely=0.1, anchor='ne', relwidth=0.6, relheight=0.75)
 
 def callforbutton(tb=trackbox, rb=resultbox):
     reqtracks(tb, rb)
 
-butsend = Button(win, text='Перевірити', bg='#acf', font='Tahoma 13', command=callforbutton)
+butsend = Button(win, text='Перевірити', bg='#acf', font='Tahoma 12', command=callforbutton)
 butsend.place(relx=0.5, rely=0.97, anchor='s')
 
 win.mainloop()
